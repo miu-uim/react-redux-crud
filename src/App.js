68dce26498101a895c70807c37301bb7e6e90c9f
miu-uim <miu.aizawa@rakus-partners.co.jp>
2021-10-22 11:10:27 +0900

    learn state and setState

--- src/App.js
+++ src/App.js
@@ -1,40 +1,31 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React, { Component } from "react";
 
-const App = () => {
-  const profiles = [
-    {
-      name: "Taro",
-      age: 10,
-    },
-    {
-      name: "Hanako",
-      age: 5,
-    },
-    {
-      name: "NoName",
-      age: 3,
-    },
-  ];
-  return (
-    <div>
-      {profiles.map((profile, index) => {
-        return <User name={profile.name} age={profile.age} key={index} />;
-      })}
-    </div>
-  );
-};
-const User = (props) => {
-  return (
-    <div>
-      Hi, I am {props.name}, and {props.age} years old!
-    </div>
-  );
-};
+const App = () => <Counter></Counter>;
 
-User.propTypes = {
-  name: PropTypes.string,
-  age: PropTypes.number.isRequired,
-};
+class Counter extends Component {
+  constructor(props) {
+    super(props);
+    console.log(this.state);
+    this.state = { count: 0 };
+  }
+
+  handlePlusButton = () => {
+    // this.state ={count:0}で直接変更やっちゃだめ。setStateを使う
+    this.setState({ count: this.state.count + 1 });
+  };
+
+  handleMinusBurron = () => {
+    this.setState({ count: this.state.count - 1 });
+  };
+  render() {
+    return (
+      <>
+        <div>count:{this.state.count}</div>
+        <button onClick={this.handlePlusButton}>+1</button>
+        <button onClick={this.handleMinusBurron}>-1</button>
+      </>
+    );
+  }
+}
 
 export default App;
